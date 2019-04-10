465fd1b72e3256b1c3d4c6c5e176bca064303ffd
L Stephens <lstephens46@my.stlcc.edu>
2019-04-10 12:14:20 -0500

    finish HighSchool pass all tests

--- HighSchool/Program.cs
+++ HighSchool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HighSchool
 {
@@ -6,6 +7,29 @@ namespace HighSchool
     {
         public static void Main()
         {
+            List<Student> studentList = new List<Student>
+            {
+                new Student {FirstName = "Kim", LastName = "Warner", Grade = 5, GPA = 3.7},
+                new Student {FirstName = "Lauren", LastName = "Stephens", Grade = 12, GPA = 3.9},
+                new Student {FirstName = "Johnathan", LastName = "Stephens", Grade = 12, GPA = 3.0},
+                new Student {FirstName = "Harry", LastName = "Strode", Grade = 7, GPA = 2.6},
+                new Student {FirstName = "Sally", LastName = "Rhoades", Grade = 11, GPA = 4.0},
+                new Student {FirstName = "John", LastName = "Stephens", Grade = 3, GPA = 2.9},
+                new Student {FirstName = "Tim", LastName = "Ema", Grade = 9, GPA = 4.0},
+                new Student {FirstName = "Jeremy", LastName = "Owens", Grade = 2, GPA = 1.0},
+                new Student {FirstName = "Trey", LastName = "Porter", Grade = 10, GPA = 3.6},
+                new Student {FirstName = "David", LastName = "Wilson", Grade = 12, GPA = 3.4},
+                new Student {FirstName = "Julie", LastName = "Sims", Grade = 4, GPA = 2.3},
+                new Student {FirstName = "Wally", LastName = "Wood", Grade = 6, GPA = 0.4},
+                new Student {FirstName = "Jimmy", LastName = "LittleJohn", Grade = 8, GPA = 3.8}
+            };
+
+            List<Student> inHighSchooList = Student.StarStudents(studentList);
+            foreach (Student student in inHighSchooList)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} {student.Grade} {student.GPA}");
+            }
+
             Console.ReadLine();
         }
     }
--- HighSchool/Student.cs
+++ HighSchool/Student.cs
@@ -1,12 +1,84 @@
 ﻿using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 
 namespace HighSchool
 {
     public class Student
     {
-        public static List<Student> StarStudents(List<Student> students)
+
+        public List<Student> Students { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private int _grade;
+
+        public int Grade
         {
+            get { return _grade; }
+
+            set
+            {
+                if (value < 1)
+                {
+                    _grade = 1;
+                }
+                else if (value > 12)
+                {
+                    _grade = 12;
+                }
+                else
+                {
+                    _grade = value;
+                }
+            }
+        }
+
+
+        private double _currentGpa;
+
+        public double GPA
+        {
+            get { return _currentGpa; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    _currentGpa = 0;
+                }
+                else if (value > 4)
+                {
+                    _currentGpa = 4;
+                }
+                else
+                {
+                    _currentGpa = value;
+                }
+            }
+
+
+        }
+
+        public static List<Student> StarStudents(List<Student> studentList)
+        {
+            //rtn a student list that only contains the students in Grade 9 -12, sorted by highest Grade, then high gpa, last name, first name
+
+            return studentList.Where(student => student.Grade >= 9 && student.GPA >= 3)
+                .OrderByDescending(student => student.Grade)
+                .ThenByDescending(student => student.GPA)
+                .ThenBy(student => student.LastName)
+                .ThenBy(student => student.FirstName).ToList();
         }
     }
+
+
 }
+   
+
+        
+
+
+
+    
